f242ee96d329677481e979da0c9136f177542923
Éderson Cássio <edersoncassiolf@outlook.com>
2023-06-17 18:19:21 -0300

    Essas gabiroba dão pau num dia, no outro não

--- src/test/java/com/derso/testes/controller/RegistroCaixaControllerTest.java
+++ src/test/java/com/derso/testes/controller/RegistroCaixaControllerTest.java
@@ -27,7 +27,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -39,8 +38,7 @@ import com.derso.testes.repository.RegistroCaixaRepository;
  * TESTE DE ENDPOINTS!
  */
 
-@WebMvcTest
-@ActiveProfiles({"test"})
+@WebMvcTest(controllers = RegistroCaixaController.class)
 public class RegistroCaixaControllerTest {
 	
 	// Mock de requisições 
